b55877eb6852ccdcf24a49d2bf3939b6cb287220
dhairya <dhairyasenjaliya@gmail.com>
2019-04-16 17:26:24 +0530

    Svg Added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
-Vue.config.devtools = false
-Vue.config.debug = false
-Vue.config.silent = true
+// Vue.config.devtools = false
+// Vue.config.debug = false
+// Vue.config.silent = true
 
 
 require('./bootstrap');

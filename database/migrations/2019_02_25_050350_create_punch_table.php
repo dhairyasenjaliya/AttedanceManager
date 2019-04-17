a71329159da07788a3e5c814bfed8019c99be926
dhairya <dhairyasenjaliya@gmail.com>
2019-04-17 12:19:33 +0530

    Time

--- database/migrations/2019_02_25_050350_create_punch_table.php
+++ database/migrations/2019_02_25_050350_create_punch_table.php
@@ -16,8 +16,8 @@ class CreatePunchTable extends Migration
         Schema::create('punches', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->timestamp('punch_in')->nullable();
-            $table->timestamp('punch_out')->nullable();           
+            $table->time('punch_in')->nullable();
+            $table->time('punch_out')->nullable();           
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users') ->onDelete('cascade');
         });

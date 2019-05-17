ccbf1f442f8f3fed26d6190699b290c9c39f7109
dhairya <dhairyasenjaliya@gmail.com>
2019-05-17 15:47:57 +0530

    SVG Size Fix

--- public/js/app.js
+++ public/js/app.js
@@ -73007,7 +73007,7 @@ var render = function() {
                                                 "data-name": "Layer 1",
                                                 xmlns:
                                                   "http://www.w3.org/2000/svg",
-                                                width: "450",
+                                                width: "220",
                                                 height: "400",
                                                 viewBox:
                                                   "0 0 865.00002 779.06422"
